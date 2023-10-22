1b897ca5ace1900a427fbec8885755ee77ac3928
Kirill Shirokov <kirillshirokovn@yandex.ru>
2023-10-22 18:26:35 +0300

    add workflow

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -114,8 +114,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 
-STATIC_URL = '/backend_static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'backend_static')
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
