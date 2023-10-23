33aa5e6cd6874a6f5e7314a23035857c3629150a
Kirill Shirokov <kirillshirokovn@yandex.ru>
2023-10-23 22:32:52 +0300

    01review

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -10,12 +10,5 @@ class IsAdminOrAuthorOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         return (request.method in permissions.SAFE_METHODS
                 or obj.author == request.user
-                or request.user.is_admin
+                or request.user.is_staff
                 )
-
-
-class AuthorOrReadOnly(IsAdminOrAuthorOrReadOnly):
-
-    def has_object_permission(self, request, view, obj):
-        return (request.method in permissions.SAFE_METHODS
-                or request.user == obj.author)
