28e216054254aa2ed928e8e99de2c4204f9518db
Kirill Shirokov <kirillshirokovn@yandex.ru>
2023-10-23 18:09:49 +0300

    01review

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,5 +1,5 @@
-from django.db.models import Sum
 from django.core.exceptions import PermissionDenied
+from django.db.models import Sum
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
